df7ead87428cbfa848d736ce6d710a6722938f43
Amit Gulati <155864118+gulati8@users.noreply.github.com>
2024-05-11 14:16:33 +0000

    add title to filter conditions for list items

--- backend/app/db/repositories/items.py
+++ backend/app/db/repositories/items.py
@@ -116,6 +116,7 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
         tag: Optional[str] = None,
         seller: Optional[str] = None,
         favorited: Optional[str] = None,
+        title: Optional[str] = None,
         limit: int = 20,
         offset: int = 0,
         requested_user: Optional[User] = None,
@@ -209,6 +210,14 @@ class ItemsRepository(BaseRepository):  # noqa: WPS214
             )
             # fmt: on
 
+        if title:
+            query_params.append(title)
+            query_params_count += 1
+
+            # fmt: off
+            query = query.where(Field("title").like("%" + Parameter(query_params_count) + "%"))
+
+            # fmt: on
         query = query.limit(Parameter(query_params_count + 1)).offset(
             Parameter(query_params_count + 2),
         )
